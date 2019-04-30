bae15ceb7e80cbd4ebe130dba41b3e85d2d93255
Ivan Rojas <irojas1997@gmail.com>
2019-04-30 19:01:05 -0400

    Using the Express Router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
-const { accounts, users, writeJSON } = require('./data');
+const { accounts, users } = require('./data');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,26 +15,10 @@ app.use(express.urlencoded(true));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
 
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
-
-app.get('/transfer', (req, res) => res.render('transfer'));
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance -= parseInt(req.body.amount, 10);
-  accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-  writeJSON();
-  res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
-app.post('/payment', (req, res) => {
-  accounts.credit.balance -= parseInt(req.body.amount, 10);
-  accounts.credit.available += parseInt(req.body.amount, 10);
-  writeJSON();
-  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
+// eslint-disable-next-line no-console
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
